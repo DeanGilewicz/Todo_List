6f9481fcfa6f9d59d1f6eb860af8eab589f77202
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-28 16:29:40 -0400

    working on total function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
+var task_manager = [];
+
+
 var Todo = function (options) {
   options = options || {};
   this.name = options.name || " ";
@@ -16,13 +19,24 @@ var Todo = function (options) {
     $(this).remove();
   };
 
-// this.total = function () {
-//
-//   _.each ()
-//   todo.length;// add up the number of items (use number property)
-// };
+
+  this.total = function () {
+    return task_manager.length;
+  };
+
+
+
 
 };
+
+
+var numbers = [1, 2, 3, 4, 5, 10];
+
+for (var i = 0; i < numbers.length; i++) {
+numbers[i] + numbers[i] + 1;
+};
+
+// getting 21 but need 25
 
 // status - complete or incomplete
 // visible - whether item appears or not
@@ -30,18 +44,33 @@ var Todo = function (options) {
 // give each item an id and use underscore sort...
 
 
-// $('When click item').on('click', function(){
-//
-//   this.comp();
-// });
-//
-// $('When add new item or delete item').on('click', function(){
-//
-//   this.total();
-// });
-//
-//
-// $('When click item to delete').on('click', function(){
-//
-//   this.visible();
-// });
+$('When click item').on('click', function(){
+
+  this.comp();
+});
+
+$('When clicked add item').on('click', function(){
+  add item to task manager - push this item
+  this.total();
+
+
+});
+
+
+$('When clicked delete item').on('click', function(){
+  remove item to task manager - remove this item
+  this.total();
+});
+
+
+$('When click item to delete').on('click', function(){
+
+  this.visible();
+});
+
+// sum = 0;
+// 0
+// for (var i = 0; i < numbers.length; i++) {
+// sum = sum + numbers[i];
+// };
+// 25
