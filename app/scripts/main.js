f15b2727039f189479660f2606b0e119dacd72c3
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-29 13:58:18 -0400

    updated template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,112 +1,127 @@
-var task_manager = [];
+var my_server = 'http://tiy-atl-fe-server.herokuapp.com/collections/testtododg1';
 
-var Todo = function (options) {
+// ToDo Constructor (Blueprint)
+var ToDo = function (options) {
   options = options || {};
-  this.name = options.name || " ";
-  this.number = options.number || 0;
-  this.status = options.status || false; // status - complete or incomplete
-  this.selected = function() {
-    if(this.status === false) {
-      this.status = true;
-    } else if(this.status === true) {
-      this.status = false;
-    }
-  };
-  this.total = function () {
-    // push each item into task_manager array
-    // return task manager.length
-
-    return task_manager.length;
-  };
-  // this.add_item =
-  // this.remove_item =
+  this.task = options.task || '';
+  this.finished = 'false';
 };
 
 
-var items_template = $('script').html();
-var rendered = _.template(items_template);
+// collection of ToDo's
+var my_todo_list;
 
-var add_item = function() {
-  var input = $('text input field').val();
-  if (input === '') {
-    return;
-  } else {
-  var item = new Todo({
-    name: input,
-    status: $(this).status
+//Setup my Template
+var task_template = $('#script_all_items').html();
+var rendered = _.template(task_template);
+
+// Grabbing ALL items and showing on page
+
+$.getJSON(my_server).done( function (data) {
+
+    my_todo_list = data;
+
+  _.each(my_todo_list, function(item) {
+    $('#todoList').append(rendered(item));
   });
-};
 
-  $('where html will be displayed').append(rendered(item));
-  $('text input field').val('');
+});
 
-};
+
+// adding new tasks to my todo list
+var task, contents;
 
 // when add button is clicked - a new item is added to list (array)
-$('When add btn is clicked').on('click', function() {
-  add.item();
+// at the moment when form is clicked = knows to look for button that has id submit
+$('#Chores').on('submit', function (event) {
+  event.preventDefault();
+  var item_self = this;
+
+  // Grag the task value
+  contents = $('#input_text').val();
+
+  // Create a new ToDo instance
+  task = new ToDo({ task: contents });
+
+  // Send to our server
+  // $.ajax({
+  //   type: 'POST',
+  //   content: JSON,
+  //   url: my_server,
+  //   data: task
+  // }).done( function(data) {
+
+    // Add to my todo_list - push to array
+    my_todo_list.push(data);
+
+    // Show our task on the page
+    $('#todoList').append(rendered(data));
+
+    // Reset my form
+    $(item_self)[0].reset();
+
+  // });
+
 });
+
+
+// Manage ToDo items - marking them complete/incomplete
+var task_manager;
+
+$('#todoList').on('click', 'li', function (event) {
+  event.preventDefault();
+
+  var myID = $(this).attr('id');
+
+  // Find the instance of my task
+  task_manager = _.findWhere(my_todo_list, { _id: myID });
+
+  //If it's complete, mark it as complete, else mark it done
+  if (task_manager.finished === 'true') {
+    task_manager.finished = 'false';
+    $(this).removeClass('complete');
+  } else {
+    task_manager.finished = 'true';
+    $(this).addClass('complete');
+  }
+
+  // $.ajax ({
+  //   type: 'PUT',
+  //   url: my_server + "/" + task_manager._id,
+  //   data: task_manager
+  // });
+
+});
+
 
 // when delete button is clicked - remove this item from list
-$('When remove btn is clicked').on('click', function() {
-  this.remove();
-});
+// $('When remove btn is clicked').on('click', function() {
+//   this.remove();
+// });
 
+// $.ajax ({
+//   type: 'DELETE',
+//   url: my_server + "/" + task_manager._id,
+//   data: todo_modifier
+// });
 
-// when actual item is clicked, change status of item (complete to uncomplete or vice versa)
-$('When actual list item is clicked').on('click', function() {
-  this.selected();
-});
-
-// when all button is clicked - show all list items
-$('When item is added').on('click', function() {
-  // show all list items
-});
-
-// when completed button is clicked - show completed list items only
-$('When list item is clicked').on('click', function() {
-  this.status();
-});
-
-// when incomplete button is clicked - show incomplete list items only
-$('When list item is clicked').on('click', function() {
-  this.selected();
-});
-
-
-//
-// $('When clicked add item').on('click', function(){
-//   add item to task manager - push this item
-//   this.total();
-//
-//
+// // FILTER - when all button is clicked - show all list items
+// $('When item is added').on('click', function() {
+//   // show all list items
 // });
 //
-//
-// $('When clicked delete item').on('click', function(){
-//   remove item to task manager - remove this item
-//   this.total();
+// // FILTER - when completed button is clicked - show completed list items only
+// $('When list item is clicked').on('click', function() {
+//   this.status();
 // });
 //
-//
-// $('When click item to delete').on('click', function(){
-//
-//   this.visible();
+// // FILTER - when incomplete button is clicked - show incomplete list items only
+// $('When list item is clicked').on('click', function() {
+//   this.selected();
 // });
-//
-// $('#btnNew').on('click', function(event){
-//   event.preventDefault();
-//   add_item();
-// });
-//
-// ('').on('click', 'li', function(event){
-//   event.preventDefault();
-//   $('.').css('display' , 'block');
-// });
-//
-// // sum = 0;
-// // 0
-// // for (var i = 0; i < numbers.length; i++) {
-// // sum = sum + numbers[i];
-// // };
-// // 25
+
+
+
+// this.total = function () {
+//   return task_manager.length;
+// };
